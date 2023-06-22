b04297b02938adb9e5346249c7871dd5ea3c5221
Beatrice Hahn <beatricehahn7@gmail.com>
2023-06-21 22:48:25 -0700

    Combine course info and chapter info into a single object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,28 +3,29 @@ import Content from './component/Content';
 import Total from './component/Total';
 
 const App = () => {
-  const title = 'Half Stack application development';
-
-  const courses = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+  const course = {
+    title: 'Half Stack application development',
+    chapters: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header title={title}/>
-      <Content courses={courses} />
-      <Total courses={courses} />
+      <Header title={course.title}/>
+      <Content courses={course.chapters} />
+      <Total courses={course.chapters} />
     </div>
   );
 };
