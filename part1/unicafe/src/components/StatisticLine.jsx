3703b0c2ff51fcd14aa5ae31184f70096db3a8c6
Beatrice Hahn <beatricehahn7@gmail.com>
2023-06-23 13:53:54 -0700

    Display each statistic in its own component

---
+++ part1/unicafe/src/components/StatisticLine.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+const StatisticLine = (props) => {
+    return <p>{props.text} {props.value}</p>
+}
+
+export default StatisticLine
--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,20 +1,26 @@
+/* eslint-disable react/prop-types */
+
+import StatisticLine from "./StatisticLine"
+
 // This component displays extracted statistics
 const Statistics = ({clicks}) => {
+    const { good, neutral, bad, total} = clicks
 
-    if (clicks.total === 0) {
+    if (total === 0) {
         return (
             <p>No statistics available</p>
         )
     } 
     return (
         <div>
-            <h1>Statistics</h1>
-            <p>good {clicks.good}</p>
-            <p>neutral {clicks.neutral}</p>
-            <p>bad {clicks.bad}</p>
-            <p>all {clicks.total}</p>
-            <p>average {(clicks.good-clicks.bad)/ clicks.total}</p>
-            <p>positive {clicks.good/(clicks.total) * 100} %</p>
+            <h2>Statistics</h2>
+
+            <StatisticLine text='good:' value={good} />
+            <StatisticLine text='neutral:' value={neutral} />
+            <StatisticLine text='bad:' value={bad} />
+            <StatisticLine text='total:' value={total} />
+            <StatisticLine text='average:' value={(good-bad)/ total} />
+            <StatisticLine text='positive:' value={`${good/(total) * 100} %`} />
         </div>
     )
     
